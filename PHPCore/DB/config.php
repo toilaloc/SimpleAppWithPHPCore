0ae2c214eb4e88a9bd05ec4f194981fb4190e991
toilaloc <locpc.qb@gmail.com>
2021-03-05 09:36:51 +0700

    push all simple fie

---
+++ PHPCore/DB/config.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$database = "phpcore";
+$username = "root";
+$password = "";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
